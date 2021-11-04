d6d14fe1d0169a56289ddb6919952c1faadeda19
botwes <botanwes@gmail.com>
2021-11-04 11:26:12 -0500

    actual stuff

--- Dice.js
+++ Dice.js
@@ -1,54 +1,80 @@
-function Init()
+const scene = new THREE.Scene();
+var camera,renderer;
+scene.background = new THREE.Color(0x000000);
+let x =1;
+let y =0;
+let z= 0;
+function init()
 {
+
+    console.log("init reached");
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth*.9, window.innerHeight*.9);
+    let candiv = document.getElementById("CanvasContainer");
+    renderer.domElement.id = "Renderer";
+    candiv.style.width=window.innerWidth*.9;
+    candiv.style.Height=window.innerWidth*.9;
+    candiv.appendChild(renderer.domElement);
+    
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+    scene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+    directionalLight.position.set(0, -10, -3); // x, y, z
+    scene.add(directionalLight);
+
+    const width = 10;
+    const height = width * (window.innerHeight / window.innerWidth);
+    camera = new THREE.OrthographicCamera(
+    width / -1, // left
+    width / 1, // right
+    height / 1, // top
+    height / -1, // bottom
+    1, // near
+    100 // far
+    );
+    camera.position.set(0, 0, -10);
+    camera.lookAt(1, 1, 0);
+    
+
+
     let D1 = NewDie(1,2,3,4,5,6);
     console.log(D1);
-    DrawDie(D1);
+    AddDie();
 }
+
 function NewDie(s1,s2,s3,s4,s5,s6)
 {
     var die = [s1,s2,s3,s4,s5,s6]
     return die;
     
 }
-// Add a cube to the scene
-
-function DrawDie(die)
+function AddDie()
 {
-
-    const geometry = new THREE.BoxGeometry(3, 1, 3); // width, height, depth
-    const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
+    let Die = NewDie(1,2,3,4,5,6);
+    const geometry = new THREE.BoxGeometry(1, 1, 1); // width, height, depth
+    const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00});
     const mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(0, 0, 0);
+    mesh.position.set(x, y, 0);
+    x=getRandomInt(-10,10);
+    y= getRandomInt(-3,3);
+    console.log("X: "+x+"Y: "+y)
     scene.add(mesh);
-
-    // Set up lights
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-    scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
-    directionalLight.position.set(10, 20, 0); // x, y, z
-    scene.add(directionalLight);
-
-    // Camera
-    const width = 10;
-    const height = width * (window.innerHeight / window.innerWidth);
-    const camera = new THREE.OrthographicCamera(
-    width / -2, // left
-    width / 2, // right
-    height / 2, // top
-    height / -2, // bottom
-    1, // near
-    100 // far
-    );
-
-    camera.position.set(4, 4, 4);
-    camera.lookAt(0, 0, 0);
-
-    // Renderer
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.render(scene, camera);
-
-    // Add it to HTML
-    document.body.appendChild(renderer.domElement);
 }
+var animate = function () 
+{
+    console.log("animate reached");
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+function getRandomInt(min, max) 
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var button = document.getElementById("start")
+button.addEventListener("click",init);
+var button1 = document.getElementById("Add")
+button1.addEventListener("click",AddDie);
+init();
+animate();
